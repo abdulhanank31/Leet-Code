8151184e731a3bb54893db23a58f8c3de76f9dd5
Abdul Hanan <95066696+abdulhanank31@users.noreply.github.com>
2026-01-22 22:14:59 +0500

    Time: 1 ms (38.35%) | Memory: 58.5 MB (6.55%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// Definition for singly-linked list.
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+var deleteDuplicates = function(head) {
+    let current = head;
+
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
